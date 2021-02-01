e3f7cb37b9d7fe9ae3fb936ea71852838b6a5872
James <james.hansung.park@gmail.com>
2021-02-01 01:20:54 -0500

    updated user auth

---
+++ frontend/src/util/session_api_util.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { logoutUser } from '../actions/session_actions';
+
+export const signup = (userData) => {
+  return axios.post('/api/users/register', userData);
+};
+
+export const login = (userData) => {
+  return axios.post('/api/users/login', userData);
+};
+
+export const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = token;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+export const logout = () => dispatch => {
+  localStorage.removeItem("jwtToken");
+  APIUtil.setAuthToken(false);
+  dispatch(logoutUser())
+}
